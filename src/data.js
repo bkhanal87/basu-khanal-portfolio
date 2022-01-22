0e5116e9d6ff765596d845a576fc1811a1ed44ae
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2022-01-22 15:11:33 -0500

    updating all changes

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const projects = [
+  {
+    title: "National-Parks",
+    subtitle: "Project-One",
+    description:
+      "This website was designed to give users an easy way to search for national parks to see while visiting a state, as well as look for different activities to do while visiting. Technologies used: HTML5, CSS3",
+    image: "./national-parks.png",
+    link: "https://bkhanal87.github.io/National-Parks/",
+  },
+
+  {
+    title: "Take-a-Break",
+    subtitle: "Project-Two",
+    description:
+      "A website for Take-a-Break restaurant where users can browse the menu, read reviews, and leave reviews. Technologies used: Node.js and Express.js- creating a RESTful API, MySQL and Sequelize ORM- database, Handlebars.js- template engine, Bootstrap- CSS framework, ScrollReveal- JS library.",
+    image: "./take-a-break.png",
+    // Repo: "https://github.com/Chtanicala/Take-a-Break",
+    link: "https://enigmatic-hamlet-26860.herokuapp.com/",
+  },
+
+  {
+    title: "Schedule-Planner",
+    subtitle: "JavaScript schedule planner",
+    description:
+      "Calendar application that allows a user to save events for each hour of the day so that they can manage their time efficiently.",
+    image: "schedule-planner.png",
+    // Repo: "https://github.com/bkhanal87/Schedule-Planner",
+    link: "https://bkhanal87.github.io/Schedule-Planner/",
+  },
+
+  {
+    title: "JavaScript-Password-Generator",
+    subtitle: "Password Generator",
+    description:
+      "To enable user to generate strong and secure password by using different combination of numbers, characters, and icons.",
+    image: "password-generator.png",
+    // Repo: "https://github.com/bkhanal87/JavaScript-Password-Generator",
+    link: "https://github.com/bkhanal87/Schedule-Planner",
+  },
+];
+
+export const skills = [
+  "HTML5",
+  "CSS3",
+  "JavaScript",
+  "React",
+  "Node",
+  "Express",
+  "SQL",
+  "MongoDB",
+];
