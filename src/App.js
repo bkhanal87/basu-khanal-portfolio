5eaea0548704ca8282e7e4672e61fa504a370800
bkhanal87@gmail.com <bkhanal87@gmail.com>
2023-09-18 22:35:34 -0400

    delete all existing code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React from "react";
-import Navbar from "./Components/Navbar";
-import About from "./Components/About";
-import Projects from "./Components/Projects";
-import Skills from "./Components/Skills";
-import Contact from "./Components/Contact";
-import Resume from "./Components/Resume";
-export default function App() {
-  return (
-    <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
-      {/* <Resume /> */}
-    </main>
-  );
+import Navigationbar from "./Components/Navigationbar";
+
+const App = () => {
+    return (
+        <div>
+            <Navigationbar />
+        </div>
+    )
 }
+
+export default App;
---
+++ src/Components/Navigationbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+const Navigationbar = () => {
+  return (
+    <h1>Hello world!</h1>
+  )
+}
+
+export default Navigationbar;
