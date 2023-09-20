57a315fe429ac3d1368f9346eefb955d882a0b2a
bkhanal87@gmail.com <bkhanal87@gmail.com>
2023-09-19 22:36:14 -0400

    add navigation logic

--- src/Components/Navigationbar.js
+++ src/Components/Navigationbar.js
@@ -1,10 +1,41 @@
 import React from "react";
 
-
-
 const Navigationbar = () => {
   return (
-    <h1>Hello world!</h1>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        Intro
+      </a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="Navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="/about">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="/projects">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="/skills">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="/contacts">
+              Contacts
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
